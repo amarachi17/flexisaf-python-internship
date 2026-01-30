6289596b92e678f0cfb880ddbc2d703372215576
confidence <confidencenkeonye@gmail.com>
2026-01-30 10:24:31 +0100

    Fixed a bug

--- week-03/decision_making.py
+++ week-03/decision_making.py
@@ -1,6 +1,6 @@
 # ----- AGE VALIDATOR ------
 
-print("\n === Age Validator ===")
+print("\n -------- Age Validator --------")
 age = int(input("Enter your age: "))
 
 if age >= 18:
@@ -10,21 +10,21 @@ else:
 
 # ----- LOGIN FLOW ------
 
-print("\n === Login Flow === ")
+print("\n --------- Login Flow ---------- ")
 correct_username = "admin"
 correct_password = "password1234"
 
 username = input("Enter username: ")
 password = input("Enter password: ")
 
-if username == correct_password and password == correct_password:
+if username == correct_username and password == correct_password:
     print("Login successful")
 else:
     print("Invalid username and password")
 
 # ------ LOAN ELIGIBILITY CHECKER -------
 
-print("\n === Loan Eligibilty Checker ===")
+print("\n ---------- Loan Eligibilty Checker ----------")
 loan_age = int(input("Enter your age: "))
 monthly_income = int(input("Enter your monthly income: "))
 employment_status = input("Are you employed? (yes/no): ").lower()
@@ -36,7 +36,7 @@ else:
 
 # ------- EXPENSE TRACKER -------
 
-print("\n === Expence Tracker ===")
+print("\n ---------- Expence Tracker ---------- ")
 spending_limit = 50000
 total_expenses = int(input("Enter your total expenses: "))
 
